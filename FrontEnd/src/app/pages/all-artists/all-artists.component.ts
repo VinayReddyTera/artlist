ef5e74c35f03e580ad645763f2dc6d61e524a407
VinayReddyTera <teravinay97@gmail.com>
2023-11-04 18:47:37 +0530

    working on artist booking

--- FrontEnd/src/app/pages/all-artists/all-artists.component.ts
+++ FrontEnd/src/app/pages/all-artists/all-artists.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ApiService } from '../services/api.service';
+import { EncryptionService } from '../services/encryption.service';
 
 @Component({
   selector: 'app-all-artists',
@@ -9,7 +11,7 @@ import { ApiService } from '../services/api.service';
 export class AllArtistsComponent implements OnInit{
 artists:any;
 
-constructor(private apiService:ApiService){}
+constructor(private apiService:ApiService,private router:Router,private encrypt:EncryptionService){}
 
 ngOnInit() {
    this.apiService.initiateLoading(true);
@@ -23,6 +25,12 @@ ngOnInit() {
                     name: person.name,
                     email: person.email,
                     phoneNo: person.phoneNo,
+                    address: person.address,
+                    mandal: person.mandal,
+                    district: person.district,
+                    state: person.state,
+                    pincode: person.pincode,
+                    language: person.language,
                     skill: skill
                 });
             });
@@ -46,6 +54,12 @@ ngOnInit() {
   ).add(()=>{
     this.apiService.initiateLoading(false)
   })
+}
+
+view(data:any){
+    localStorage.setItem('artistData',this.encrypt.enCrypt(JSON.stringify(data)));
+    // this.router.navigateByUrl('artist-data');
+    window.open(`${window.location.origin}/artist-data`)
 }
 
 }
--- FrontEnd/src/app/pages/artist-data/artist-data.component.ts
+++ FrontEnd/src/app/pages/artist-data/artist-data.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EncryptionService } from '../services/encryption.service';
 
 @Component({
   selector: 'app-artist-data',
   templateUrl: './artist-data.component.html',
   styleUrls: ['./artist-data.component.css']
 })
-export class ArtistDataComponent {
+export class ArtistDataComponent implements OnInit{
+
+  constructor(private router:Router,private decrypt: EncryptionService){}
+
+  artistData:any;
+
+  ngOnInit(): void {
+    if(localStorage.getItem('artistData')){
+      this.artistData = JSON.parse(this.decrypt.deCrypt(localStorage.getItem('artistData')));
+    }
+    else{
+      this.router.navigateByUrl('all-artists')
+    }
+    console.log(this.artistData)
+  }
 
 }
