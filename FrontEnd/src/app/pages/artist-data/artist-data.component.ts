b393329176bc69506b29b020baadecf8baf7c75a
VinayReddyTera <teravinay97@gmail.com>
2023-11-05 10:30:03 +0530

    working on booking

--- FrontEnd/src/app/pages/artist-data/artist-data.component.ts
+++ FrontEnd/src/app/pages/artist-data/artist-data.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../services/api.service';
 import { EncryptionService } from '../services/encryption.service';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-artist-data',
@@ -10,10 +11,12 @@ import { EncryptionService } from '../services/encryption.service';
 })
 export class ArtistDataComponent implements OnInit{
 
-  constructor(private router:Router,private decrypt: EncryptionService,private apiservice:ApiService){}
+  constructor(private router:Router,private decrypt: EncryptionService,
+    private apiservice:ApiService,private fb: FormBuilder){}
 
   artistData:any;
-  apiCalled:boolean=false
+  apiCalled:boolean=false;
+  bookingForm:any;
 
   ngOnInit(): void {
     if(localStorage.getItem('artistData')){
@@ -23,7 +26,14 @@ export class ArtistDataComponent implements OnInit{
       this.router.navigateByUrl('all-artists')
     }
     console.log(this.artistData)
+    this.bookingForm = this.fb.group({
+      type:['',[Validators.required]],
+      date : ['',[Validators.required]]
+    })
   }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.bookingForm.controls; }
 
   fetchAvailable(){
     if(!this.apiCalled){
@@ -51,6 +61,41 @@ export class ArtistDataComponent implements OnInit{
         this.apiservice.initiateLoading(false);
       })
     }
+  }
+
+  bookNow(){
+    if(this.bookingForm.valid){
+      this.apiservice.initiateLoading(true);
+      this.apiservice.login(this.bookingForm.value).subscribe(
+      (res : any)=>{
+        console.log(res)
+        if(res.status == 200){
+        }
+        else if(res.status == 204){
+        let msgData = {
+              severity : "error",
+              summary : 'Error',
+              detail : res.data,
+              life : 5000
+            }
+            this.apiservice.sendMessage(msgData);
+        }
+      },
+      (err:any)=>{
+        console.log(err);
+      }
+    ).add(()=>{
+      this.apiservice.initiateLoading(false)
+    })
+  }
+  else{
+    const controls = this.bookingForm.controls;
+    for (const name in controls) {
+        if (controls[name].invalid) {
+            controls[name].markAsDirty()
+        }
+    }
+  }
   }
 
 }
