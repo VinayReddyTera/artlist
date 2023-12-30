1eca556c5e6bbf31bdea04b1d34682f549bd32fb
VinayReddyTera <teravinay97@gmail.com>
2023-12-30 07:17:12 +0530

    changes in domain

--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
   secretKey : "M@ckiNterViiew",
-  domain : "http://localhost:8000",
+  // domain : "http://localhost:8000",
+  domain : "https://artlist-server.vercel.app",
   emailRegex : /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
   phoneNoRegex : /^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/,
   nameRegex : /^[A-Za-z\s'\-]+$/
