8c0851d326524a798662146136fcecfb7ffdb60f
VinayReddyTera <teravinay97@gmail.com>
2023-12-30 12:05:25 +0530

    vercel

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   secretKey : "M@ckiNterViiew",
-  domain : "https://artlist-server.vercel.app/",
+  domain : "",
   emailRegex : /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
   phoneNoRegex : /^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/,
   nameRegex : /^[A-Za-z\s'\-]+$/
